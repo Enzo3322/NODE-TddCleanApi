678298ee2fc09af3e81ebcac30f9c0eef0853cfe
Enzo3322 <enzo.spag14@gmail.com>
2022-12-15 22:46:43 -0300

    refactor: ensure singUpController.handle implement correct interfaces

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,5 +1,6 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
 export class SingUpController {
-	handle(httpRequest: any): any {
+	handle(httpRequest: HttpRequest): HttpResponse {
 		if (!httpRequest.body.name) {
 
 			return {

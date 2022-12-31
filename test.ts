b13b46327acbc8acfcd2d3cc9b7355d3b6e036e8
Enzo3322 <enzo.spag14@gmail.com>
2022-12-31 10:00:16 -0300

    feat: add express

--- test.ts
+++
@@ -1,7 +0,0 @@
-const returnRandomPositionOfArray = (arr: any[]) => {
-	return arr[Math.floor(Math.random() * arr.length)];
-}
-
-const filterArrayByProperty = (arr, property, value) => {
-	return arr.filter((item) => item[property] === value);
-}
